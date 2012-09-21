45b63bd238e64047009595b39a3a311d74a47fe8
root <root@localhost.localdomain>
2012-09-21 19:57:16 +0200

    Moved things

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 cd rpms
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 
 ## wget ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/a52dec-0.7.4-8.el6.rf.x86_64.rpm
 ## wget http://pkgs.repoforge.org/faac/faac-1.26-1.el6.rf.x86_64.rpm
@@ -12,26 +13,25 @@ wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
 wget http://download1.rpmfusion.org/free/el/updates/6/i386/rpmfusion-free-release-6-1.noarch.rpm
 wget http://download1.rpmfusion.org/nonfree/el/updates/6/i386/rpmfusion-nonfree-release-6-1.noarch.rpm
-
-
-rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 rpm -i --nosignature rpmfusion-free-release-6-1.noarch.rpm 
 rpm -i --nosignature rpmfusion-nonfree-release-6-1.noarch.rpm 
+
 yum update
 
 ## wget http://pkgs.repoforge.org/gtkimageview/gtkimageview-1.6.3-1.el6.rf.x86_64.rpm
-rpm -i --nosignature gtkimageview-1.6.3-1.el6.rf.x86_64.rpm
+#rpm -i --nosignature gtkimageview-1.6.3-1.el6.rf.x86_64.rpm
 
-yum install java-1.6.0-openjdk
-yum install gpac
-yum install gtkimageview
-yum install ImageMagick
-yum install lame
-yum install libmad
-yum install librtmp
-yum install opencore-amr
-yum install x264
-yum install xvidcore
-yum install ffmpeg
+yum install java-1.6.0-openjdk gpac gtkimageview lame x264  perl-Image-ExifTool libreoffice-headless
+#exiftool, open office, newer version of image magick
+#yum install gpac
+#yum install gtkimageview
+#yum install ImageMagick
+#yum install lame
+#yum install libmad
+#yum install librtmp
+#yum install opencore-amr
+#yum install x264
+#yum install xvidcore
+#yum install ffmpeg
 
-sh misc/install_entermedia.sh
+sh ./misc/install_entermedia.sh
