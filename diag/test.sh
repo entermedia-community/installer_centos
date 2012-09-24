eceec34e4e69b7d6d70c12f0b0e1e6dfa4070ba6
root <root@entermedia.(none)>
2012-09-24 17:11:22 -0400

    clean up

--- diag/test.sh
+++ diag/test.sh
@@ -19,35 +19,24 @@ echo $VER;
 exit 0;
 fi
 
-convert baby.jpg  bout.jpg
-assert bout.jpg
+mkdir results
+convert baby.jpg  results/01bout.jpg
 
-convert IMG_0192.CR2  out.jpg
-assert out.jpg
+convert IMG_0192.CR2  results/02out.jpg
 
-convert cc.tif tifout.jpg
-assert tifout.jpg
+convert cc.tif results/03tifout.jpg
 
-ffmpeg -i cb1sampleiTunes4.mov -y -acodec libfaac -ab 96k -ar 44100 -ac 1 -vcodec libx264 -vpre normal -crf 28 -threads 0 -s 704x480 outtmp.mp4
-assert outtmp.mp4
+ffmpeg -i cb1sampleiTunes4.mov -y -acodec libfaac -ab 96k -ar 44100 -ac 1 -vcodec libx264 -vpre normal -crf 28 -threads 0 -s 704x480 results/04outtmp.mp4
 
-qt-faststart results/outtmp.mp4 qtout.mp4
-assert qtout.mp4
+qt-faststart results/04outtmp.mp4 results/05qtout.mp4
 
-ffmpeg -ss 2 -deinterlace -i cb1sampleiTunes4.mov  -y -vframes 1 -f mjpeg ffmpegout.jpg
-assert ffmpegout.jpg
+ffmpeg -ss 2 -deinterlace -i cb1sampleiTunes4.mov  -y -vframes 1 -f mjpeg results/06ffmpegout.jpg
 
-convert -density 200 agenda.pdf[0]  -colorspace rgb -strip pdfout.jpg
-assert pdfout.jpg
+convert -density 200 agenda.pdf[0]  -colorspace rgb -strip results/07pdfout.jpg
 
 soffice --headless --nologo --norestore --convert-to pdf:writer_pdf_Export Office.doc 
-assert Office.pdf
+mv Office.pdf results/08office.pdf
 
-cp Office.doc Office2.doc
-soffice -headless -nologo -norestore -convert-to pdf:writer_pdf_Export Office2.doc
-#convert Office.doc -> Office2.pdf using writer_pdf_Export
-assert Office2.pdf
 
 exiftool -canon IMG_0192.CR2 -w txt
-assert IMG_0192.txt
-
+mv IMG_0192.txt results/09IMG_0192.txt
--- installer/install64.sh
+++ installer/install64.sh
@@ -4,43 +4,24 @@ mkdir downloads
 cd downloads
 
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm -O epel-release-6-7.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm -O remi-release-6.rpm 
-rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+rpm -Uvh epel-release-6*.rpm
 
-## wget ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/a52dec-0.7.4-8.el6.rf.x86_64.rpm
-## wget http://pkgs.repoforge.org/faac/faac-1.26-1.el6.rf.x86_64.rpm
-## wget http://pkgs.repoforge.org/faad2/faad2-2.7-1.el6.rf.x86_64.rpm
-## wget http://dl.atrpms.net/el6-x86_64/atrpms/stable/ffmpeg-0.10.2-54.el6.x86_64.rpm
-## No work. wget http://pkgs.org/centos-6-rhel-6/rpmfusion-free-updates-testing-x86_64/gpac-doc-0.5.0-1.el6.x86_64.rpm
+wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm -O rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 
-wget http://download1.rpmfusion.org/free/el/updates/6/i386/rpmfusion-free-release-6-1.noarch.rpm -O rpmfusion-free-release-6-1.noarch.rpm
-wget http://download1.rpmfusion.org/nonfree/el/updates/6/i386/rpmfusion-nonfree-release-6-1.noarch.rpm -O rpmfusion-nonfree-release-6-1.noarch.rpm
-rpm -i --nosignature rpmfusion-free-release-6-1.noarch.rpm 
-rpm -i --nosignature rpmfusion-nonfree-release-6-1.noarch.rpm 
+wget http://apt.sw.be/RPM-GPG-KEY.dag.txt -O RPM-GPG-KEY.dag.txt
+rpm --import RPM-GPG-KEY.dag.txt
+
+rpm -ivh rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
+
 
 yum update
 
+yum install lame  perl-Image-ExifTool libreoffice-headless libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw x264 ffmpeg
 
+cd ../rpms
+yum install ImageMagick-6.7.6.8-9.el6.x86_64.rpm ufraw-*
 
-cd ..
-yum install rpms/*.rpm
-
-
-yum install java-1.6.0-openjdk gpac lame  perl-Image-ExifTool libreoffice-headless libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw 
-
-#cp -rp misc/qt-faststart /usr/bin
- 
-#exiftool, open office, newer version of image magick
-#yum install gpac
-#yum install gtkimageview
-#yum install ImageMagick
-#yum install lame
-#yum install libmad
-#yum install librtmp
-#yum install opencore-amr
-#yum install x264
-#yum install xvidcore
-#yum install ffmpeg
-cd ./misc
+cd ../misc
+cp -rp qt-faststart /usr/bin
 sh ./install_entermedia.sh
 
