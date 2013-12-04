bd898096c50588f31296debbe179d88c46df808a
Christopher Shanti Burkey <cburkey@entermediasoftware.com>
2013-12-04 12:45:01 -0500

    Added failover scripts

--- failover/old/statuscheck.sh
+++
@@ -1,8 +0,0 @@
-if /usr/bin/wget "http://10.0.251.185/crossdomain.xml" --timeout 30 -O - 2>/dev/
-null | grep "macromedia" > /dev/null 2>&1
-then
-  echo "Production is Up";/etc/alm/productionsup.sh
-else
-  echo "Production is Down";/etc/alm/productionsdown.sh
-fi
-
