d8fef676215eac8b38207b15bc154696057adc47
Christopher Burkey <cburkey@openedit.org>
2014-03-06 12:22:17 -0500

    Added sbin

--- diag/bench.sh
+++ diag/bench.sh
@@ -7,7 +7,7 @@ cputime="$( TIMEFORMAT='%lU';time ( echo '2^2^20' | bc ) 2>&1 1>/dev/null )"
 
 videotime="$( TIMEFORMAT='%lU';time ( avconv -i artbeats_red_m480pBIG.mp4 -y -acodec libfaac -ab 96k -ar 44100 -ac 1 -vcodec libx264 -crf 28 -threads 2 -s 540x360 video.mp4tmp.mp4 2>&1 1>/dev/null ) 2>&1 1>/dev/null )"
 
-hdtime="$( hdparm -Tt /dev/sda )"
+hdtime="$( /sbin/hdparm -Tt /dev/sda )"
 
 echo "HD: $hdtime (higher is better)"
 echo "CPU: $cputime (lower is better)"
