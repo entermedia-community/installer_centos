e2ff18827856f28ca09e0742d4cff42c368e5976
Christopher Burkey <cburkey@openedit.org>
2014-04-11 15:53:36 -0400

    Added avconv mint build

---
+++ installer/misc/install_avconvMINT.sh
@@ -0,0 +1,58 @@
+#Remove old programs and create a workspace
+apt-get remove ffmpeg x264 libx264-dev
+cd /tmp
+mkdir avconvupgrade
+cd avconvupgrade
+
+apt-get install gcc yasm make libvorbis-dev libfaac0 libmp3lame0 build-essential
+
+#Orbis Support
+curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz
+tar xzvf libogg-1.3.1.tar.gz
+cd libogg-1.3.1
+./configure --prefix="/usr" --disable-shared
+make
+make install
+make distclean
+cd ..
+
+#Vorbis audio encoder
+curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz
+tar xzvf libvorbis-1.3.3.tar.gz
+cd libvorbis-1.3.3
+./configure --prefix="/usr" --with-ogg="/usr" --disable-shared
+make
+make install
+cd ..
+
+#Upgrade x264
+wget ftp://ftp.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
+tar jxvf last_x264.tar.bz2
+cd x264-snapshot-*
+./configure --enable-shared --prefix=/usr
+make
+sudo make install
+x264 --version
+ldconfig
+cd ..
+
+#Install libvpx (for WebM)
+git clone http://git.chromium.org/webm/libvpx.git
+cd libvpx*
+./configure
+make
+sudo make install
+vpxenc
+cd ..
+
+#Install avconv
+curl "http://git.libav.org/?p=libav.git;a=snapshot;h=HEAD;sf=tgz" | tar zxv
+cd libav-HEAD-*
+./configure --enable-libx264 --enable-libfaac --enable-libmp3lame --enable-gpl --enable-nonfree --enable-libvpx --enable-libvorbis
+
+make
+make install
+avconv
+
+#Remove workspace
+#rm -Rf /tmp/avconvupgrade
