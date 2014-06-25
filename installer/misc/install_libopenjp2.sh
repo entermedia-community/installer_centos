4f58df3e06bf711f036cb580f8ea038714926f30
Chris Rose <chris.rose@openedit.org>
2014-06-25 12:24:06 -0400

    More updates to install openjp2 script

--- installer/misc/install_libopenjp2.sh
+++ installer/misc/install_libopenjp2.sh
@@ -1,2 +1,12 @@
-cp ./libopenjp2/libopenjp2* /usr/lib/
-cp ./libopenjp2/pkgconfig/libopenjp2.pc /usr/lib64/pkgconfig/
+cd ./libopenjp2
+
+# Copy .pc and .so files
+cp ./usr/lib64/pkgconfig/libopenjp2.pc /usr/lib64/pkgconfig/
+cp ./usr/lib/libopenjp2* /usr/lib/
+
+# Copy Header Files
+if [ ! -d "/usr/include/openjpeg-2.1" ]; then
+  mkdir /usr/include/openjpeg-2.1
+fi
+
+cp ./usr/include/openjpeg-2.1/* /usr/include/openjpeg-2.1/
