56a80913e404af1cb2be6a3498ed3f28a3c0ea75
Chris Rose <chris.rose@openedit.org>
2014-09-29 09:43:51 -0400

    Install jdk 1.7 instead of 1.6

--- installer/install64.sh
+++ installer/install64.sh
@@ -18,7 +18,7 @@ rpm -ivh rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 
 yum update
 
-yum install -y java-1.6.0-openjdk lame  perl-Image-ExifTool libreoffice-headless libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw x264 ffmpeg unzip git
+yum install -y java-1.7.0-openjdk lame  perl-Image-ExifTool libreoffice-headless libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw x264 ffmpeg unzip git
 
 cd ../rpms
 rpm -ihv --force --nodeps ImageMagick-*
