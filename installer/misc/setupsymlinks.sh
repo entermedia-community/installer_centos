6ca1b15327376e3f883d7b242a81bd0fde0b7a46
Christopher Rose <chrisrose708@gmail.com>
2015-01-15 18:57:01 -0500

    Added setupsymlinks.sh for Newscorp

---
+++ installer/misc/setupsymlinks.sh
@@ -0,0 +1,23 @@
+# Create symbolic links
+
+cd /media
+ln -s git/entermedia-customisations/deployed/entermedia entermedia
+
+cp -r git/entermedia-customisations/data .
+
+mkdir local
+cd local
+cp -f ../git/entermedia-customisations/webapp/WEB-INF/oemounts.xml .
+
+cd /opt
+
+rm -rf entermedia
+ln -s /media/entermedia entermedia
+
+cd /opt/entermedia/webapp/WEB-INF
+
+rm -rf ./data
+rm -f ./oemounts.xml
+
+ln -s /media/data data
+ln -s /media/local/oemounts.xml oemounts.xml
