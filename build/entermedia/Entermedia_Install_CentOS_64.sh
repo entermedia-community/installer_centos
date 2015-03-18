9541ac1fd0f1779dd393161ebc03d8ad61044db5
chrisomatic <chrisrose708@gmail.com>
2015-03-18 15:22:15 -0400

    Mods to install script

--- build/entermedia/Entermedia_Install_CentOS_64.sh
+++ build/entermedia/Entermedia_Install_CentOS_64.sh
@@ -1,15 +1,11 @@
-yum -y update
-yum install -y automake
-yum install -y ufraw-* lcms2
-yum install -y java-1.7.0-openjdk lame  perl-Image-ExifTool libreoffice-headless libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw unzip git
-yum install -y ilmbase OpenEXR-libs libtool-ltdl librsvg2 libwmf-lite libXt libgomp libtiff faac ghostscript
+yum install -y automake ufraw-*
+yum install -y libreoffice-headless libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw unzip wget git
+yum install -y ilmbase OpenEXR-libs libtool-ltdl librsvg2 libwmf-lite libXt libgomp libtiff
 
-cd ../misc
-cp -p qt-faststart /usr/bin
+mv qt-faststart /usr/bin
 ln -s /usr/bin/avprobe /usr/bin/ffprobe
 
 ##make sure you have a user useradd entermedia 
-
 adduser entermedia
 usermod -a -G vboxsf entermedia
 
@@ -19,24 +15,28 @@ echo "entermedia      soft    nofile  409600" >> /etc/security/limits.conf
 echo "entermedia      hard    nofile  1024000" >> /etc/security/limits.conf
 sysctl -p
 #End Fix File Limits
-mkdir /home/entermedia/.ffmpegn
-cp .ffmpeg/libx264-normal.ffpreset /home/entermedia/.ffmpeg/libx264-normal.ffpreset
+
+mkdir /home/entermedia/.ffmpeg
+mv libx264-normal.ffpreset /home/entermedia/.ffmpeg/libx264-normal.ffpreset
 
 chown -R entermedia:entermedia /home/entermedia/.ffmpeg
 
-cp tomcat /etc/init.d/
+mv tomcat /etc/init.d/
 chkconfig --add tomcat
 chkconfig --level 3 tomcat on
 chkconfig --level 5 tomcat on
 
+cd /opt/entermedia/
+wget https://github.com/entermedia-community/tomcat8/archive/master.zip -O tomcat.zip
+unzip tomcat.zip
+rm -f tomcat.zip
+
 mkdir -p /opt/entermedia/tomcat/logs
 mkdir -p /opt/entermedia/webapp
-cp -rp ../entermedia/* /opt/entermedia/
-cp -p ../misc/delegates.xml /etc/ImageMagick/delegates.xml
 
 cd /opt/entermedia/webapp
 wget http://dev.entermediasoftware.com/jenkins/view/Demo/job/demoall/lastSuccessfulBuild/artifact/deploy/ROOT.war -O ROOT.war
-unzip ROOT.war 
+unzip ROOT.war
 rm ROOT.war
 
 #Copy data directory out and link it
