17ec9b496bfe61c753c259dc84d25113dde2219a
root <root@tesla.(none)>
2015-07-21 12:49:06 -0400

    update bench.sh

--- diag/bench.sh
+++ diag/bench.sh
@@ -1,11 +1,11 @@
 #install tools and run as root
 #yum install hdparm bc
 
-wget http://demo.entermediasoftware.com/node2/artbeats_red_m480pBIG.mp4 -nc
+wget http://demo.entermediasoftware.com/media/catalogs/public/downloads/originals/2012/10/artbeats_red_m480pSMALL.mov/artbeats_red_m480pSMALL.mov -nc
 
 cputime="$( TIMEFORMAT='%lU';time ( echo '2^2^20' | bc ) 2>&1 1>/dev/null )"
 
-videotime="$( TIMEFORMAT='%lU';time ( avconv -i artbeats_red_m480pBIG.mp4 -y -acodec libfaac -ab 96k -ar 44100 -ac 1 -vcodec libx264 -crf 28 -threads 2 -s 540x360 video.mp4tmp.mp4 2>&1 1>/dev/null ) 2>&1 1>/dev/null )"
+videotime="$( TIMEFORMAT='%lU';time ( avconv -i artbeats_red_m480pSMALL.mov -y -acodec libfaac -ab 96k -ar 44100 -ac 1 -vcodec libx264 -crf 28 -threads 2 -s 540x360 video.mp4tmp.mp4 2>&1 1>/dev/null ) 2>&1 1>/dev/null )"
 
 hdtime="$( /sbin/hdparm -Tt /dev/sda )"
 
